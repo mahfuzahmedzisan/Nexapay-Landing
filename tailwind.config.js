fd33db584042a4f76d3385ccbc39ce02c9fd976c
Mahfuz Ahmed zisan <dev.mahfuz2025@gmail.com>
2025-02-25 15:43:39 +0600

    testimonial and partner section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,7 @@ module.exports = {
       fontFamily: {
         "Noto-Sans": ["Noto Sans", "sans-serif"],
         Poppins: ["Poppins", "sans-serif"],
+        FontAwesome: ["FontAwesome"]
       },
       fontSize: {
         "heading-1": ["48px", "64px"],
